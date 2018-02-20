fba0a5bcacb440f0fb455f77565f3294510ff5c2
novalina.hutabarat <novalina.hutabarat>
2018-02-20 21:52:03 +0700

    Commit semua

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class PageController {
+	@RequestMapping("/hello")
+	public String index()
+	{
+		return "hello";
+	}
+	
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name", required=false, 
+	defaultValue="dunia") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+	
+	@RequestMapping(value= {"/greeting","/greeting/{name}"})
+	public String greetingPath(@PathVariable Optional<String> name, Model model) {
+		if(name.isPresent()) {
+			model.addAttribute("name", name.get());
+		}else {
+			model.addAttribute("name", "apap");
+		}
+		return "greeting";
+	}
+	
+	@RequestMapping("/perkalian")
+	public String perkalian(@RequestParam(value="bil1")  Optional<String> bil1, @RequestParam(value="bil2") Optional<String> bil2, Model model) {
+		if(bil1.isPresent()) {
+			model.addAttribute("bil2", "0");
+		}else if(bil2.isPresent()) {
+			model.addAttribute("bil1", "0");
+		}
+		//model.addAttribute("name", name);
+		return "perkalian";
+	}
+}
